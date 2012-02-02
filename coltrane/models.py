f3dc1642ef62aebbed02f127673aae6d5c0a1cef
root <root@debian>
2012-02-01 21:49:05 -0500

    Fixed coltrane generic object

--- coltrane/models.py
+++ coltrane/models.py
@@ -5,10 +5,18 @@ from django.contrib.auth.models import User
 from tagging.fields import TagField
 from markdown import markdown
 
+class LiveEntryManager(models.Manager):
+    def get_query_set(self):
+        return super(LiveEntryManager, self).get_query_set().filter(status=self.model.LIVE_STATUS)
+
 class Category(models.Model):
-	title =models.CharField(max_length=250, help_text='Maximum 250 characters.')
-	slug = models.SlugField(unique=True,help_text='Suggested value automatically generated from title. Must be unique')
-	description = models.TextField()
+    title =models.CharField(max_length=250, help_text='Maximum 250 characters.')
+    slug = models.SlugField(unique=True,help_text='Suggested value automatically generated from title. Must be unique')
+    description = models.TextField()
+
+    def live_entry_set(self):
+        from coltrane.models import Entry
+        return self.entry_set.filter(status=Entry.LIVE_STATUS)
 
 	class Meta:
 		ordering = ['title']
@@ -19,50 +27,54 @@ class Category(models.Model):
 
 	def __unicode__(self):
 		return self.title
+
         
 class Entry(models.Model):
-	LIVE_STATUS = 1
-	DRAFT_STATUS = 2
-	HIDDEN_STATUS = 3
-	STATUS_CHOICES = (
+    LIVE_STATUS = 1
+    DRAFT_STATUS = 2
+    HIDDEN_STATUS = 3
+    STATUS_CHOICES = (
 			(LIVE_STATUS, 'Live'),
 			(DRAFT_STATUS, 'Draft'),
 			(HIDDEN_STATUS, 'Hidden'),
 			)
-	status = models.IntegerField(choices=STATUS_CHOICES, default=LIVE_STATUS)
-	title = models.CharField(max_length=250)
-	excerpt = models.TextField(blank=True)
-	body = models.TextField()
-	pub_date = models.DateTimeField(default=datetime.datetime.now)
-	slug = models.SlugField(unique_for_date='pub_date')
-	author = models.ForeignKey(User)
-	enable_comments = models.BooleanField(default=True)
-	featured = models.BooleanField(default=False)
-	categories = models.ManyToManyField(Category)
-	tags = TagField()
+    status = models.IntegerField(choices=STATUS_CHOICES, default=LIVE_STATUS)
+    live = LiveEntryManager()
+    objects = models.Manager()
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(blank=True)
+    body = models.TextField()
+    pub_date = models.DateTimeField(default=datetime.datetime.now)
+    slug = models.SlugField(unique_for_date='pub_date')
+    author = models.ForeignKey(User)
+    enable_comments = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
+    categories = models.ManyToManyField(Category)
+    tags = TagField()
 
-	excerpt_html = models.TextField(editable=False, blank=True)
-	body_html = models.TextField(editable=False, blank=True)
+    excerpt_html = models.TextField(editable=False, blank=True)
+    body_html = models.TextField(editable=False, blank=True)
 
-	class Meta:
-		verbose_name_plural = 'Entries'
-		ordering = ['-pub_date']
+    class Meta:
+        verbose_name_plural = 'Entries'
+        ordering = ['-pub_date']
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
 
-	def save(self, force_insert=False, force_update=False):
-		self.body_html = markdown(self.body)
-		if self.excerpt:
-			self.excerpt_html = markdown(self.excerpt)
-		super(Entry, self).save(force_insert, force_update)
-	
- 	def get_absolute_url(self):
-		return ('coltrane_entry_detail', (), { 'year': self.pub_date.strftime("%Y"),
-							'month': self.pub_date.strftime("%b").lower(),
-							'day': self.pub_date.strftime("%d"),
-							'slug': self.slug })
-		get_absolute_url = models.permalink(get_absolute_url)
+    def save(self, force_insert=False, force_update=False):
+        self.body_html = markdown(self.body)
+        if self.excerpt:
+            self.excerpt_html = markdown(self.excerpt)
+        super(Entry, self).save(force_insert, force_update)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('coltrane_entry_object_detail', (), { 'year': self.pub_date.strftime("%Y"),
+                            'month': self.pub_date.strftime("%b").lower(),
+                            'day': self.pub_date.strftime("%d"),
+                            'slug': self.slug })
+    #    get_absolute_url = models.permalink(get_absolute_url)
 
 	
 class Link(models.Model):
--- coltrane/views.py
+++ coltrane/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import get_object_or_404, render_to_response
 from coltrane.models import Entry, Category
 from django.views.generic.list_detail import object_list
 
-def entries_index(request):
-	return render_to_response('coltrane/entry_index.html',
-					{'entry_list': Entry.objects.all() }
-					)
 
 def category_detail(request, slug):
 	category = get_object_or_404(Category, slug=slug)
